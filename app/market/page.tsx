ec502e8399f2b378f57e43bd6fde86ad949a88b7
Roman Emiliano Arenas <98709881+RomanArenas32@users.noreply.github.com>
2024-12-26 18:34:15 -0300

    add-shadcn

--- app/market/page.tsx
+++ app/market/page.tsx
@@ -3,16 +3,26 @@
 import { CustomerHome } from "@/components/customer/home"
 import { SellerHome } from "@/components/seller/home"
 import { RoleSwitch } from "@/components/ui/role-swich"
+import { ChevronLeft, Search } from "lucide-react"
 import { useState } from "react"
 
 export default function Page() {
-  const [role, setRole] = useState<"customer" | "seller">("customer")
+    const [role, setRole] = useState<"customer" | "seller">("customer")
 
-  return (
-    <div className="flex flex-col items-center gap-8 p-4">
-      <RoleSwitch role={role} onRoleChange={setRole} />
-      {role === "customer" ? <CustomerHome /> : <SellerHome />}
-    </div>
-  )
+    return (
+        <div>
+            <div className="h-[56px] flex items-center gap-4 p-4 justify-between">
+                <ChevronLeft /> 
+                <h2>Emprende ya</h2>
+                <Search />
+            </div>
+           
+            <div className="flex flex-col items-center gap-8 p-4">
+                <RoleSwitch role={role} onRoleChange={setRole} />
+                {role === "customer" ? <CustomerHome /> : <SellerHome />}
+            </div>
+        </div>
+
+    )
 }
 
