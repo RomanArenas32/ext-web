c7a6bcafd26031c4a74714dfbd7d3e98f54418d3
Roman Emiliano Arenas <98709881+RomanArenas32@users.noreply.github.com>
2024-12-27 21:01:58 -0300

    fix

--- app/(dashboard)/chats/[id]/page.tsx
+++ app/(dashboard)/chats/[id]/page.tsx
@@ -1,49 +1,26 @@
-"use client"
+'use client'
 
 import { Separator } from "@/components/ui/separator"
-import { ChevronLeft } from "lucide-react"
-import { useState } from "react"
-import ChatList from "@/components/chat/chatea"
-import ChatDetail from "@/components/chat/chatbox"
-import OrderDetail from "@/components/chat/ventarealizada"
-import ChatWithWarning from "@/components/chat/chatfinal"
+import { ChevronLeft } from 'lucide-react'
 import Link from "next/link"
+import ChatWithWarning from "@/components/chat/chatfinal"
 
-export default function Page() {
+export default function ChatPage() {
+  return (
+    <div className="rounded-lg overflow-hidden shadow-lg">
+      <div className="bg-green-800 text-white p-6 flex items-center justify-between relative">
+        <Link href="/">
+          <ChevronLeft className="text-white w-6 h-6" />
+        </Link>
+        <h1 className="text-lg font-bold">Chats</h1>
+      </div>
 
-    return (
-        <div className="rounded-lg overflow-hidden shadow-lg">
-            <div className="bg-green-800 text-white p-6 flex items-center justify-between relative">
-                <Link href={`/`}>
-
-                    <button className="absolute left-4">
-                        <ChevronLeft className="text-white w-6 h-6" />
-                    </button>
-                </Link>
-
-                <h1 className="text-lg font-bold pl-[160px]">Chats</h1>
-            </div>
-
-            <Separator />
-            {/*
-            
-               <div className="flex flex-row items-center gap-8 p-4 justify-between">
-                <ChatList />
-            </div>
-            <div className="flex flex-row items-center gap-8 p-4 pt-20 justify-between">
-                <ChatDetail />
-            </div>
-            <div className="flex flex-row items-center gap-8 p-4 pt-20 justify-between">
-                <OrderDetail />
-            </div>
-
-            */}
-         
-            <div className="flex flex-row items-center gap-8 p-4 pt-20 justify-between">
-                <ChatWithWarning />
-            </div>
-        </div>
-    )
+      <Separator />
+      
+      <div className="p-4 pt-20">
+        <ChatWithWarning />
+      </div>
+    </div>
+  )
 }
-
 
